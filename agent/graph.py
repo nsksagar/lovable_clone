db87efeffc136e6a99e02d1d68d5ca2e75e52a8e
Sagar Nookarapu <193294266+nsksagar@users.noreply.github.com>
2026-06-11 23:53:23 -0400

    small updates

--- agent/graph.py
+++ agent/graph.py
@@ -4,17 +4,29 @@ from dotenv import load_dotenv
 from prompts import *
 from states import *
 
+from langgraph.constants import END
+from langgraph.graph import StateGraph
+
 
 user_prompt = 'create a simple calculator web application'
-
-prompt = planner_prompt(user_prompt)
 
 load_dotenv()
 
 llm = ChatGroq(model = 'llama-3.3-70b-versatile')
 
+def planner_agent(state: dict)-> dict:
+    user_prompt = state['user_prompt']
+    resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    return {"plan" : resp}
 
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.set_entry_point('planner')
 
-resp = llm.with_structured_output(Plan).invoke(prompt)
+agent = graph.compile()
 
-print(resp)
+user_prompt = "create a simple calculator web application"
+
+result = agent.invoke({"user_prompt" : user_prompt})
+
+print(result)
--- agent/states.py
+++ agent/states.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field , ConfigDict
 
 class File(BaseModel):
     path : str = Field(
@@ -24,4 +24,15 @@ class Plan(BaseModel):  # Schema : <class '__main__.schema'>
     files: list[File] = Field(
         description = "A list of files to be created, each each with a 'path' and 'purpose'"
     )
+
+
+class ImplementationTask(BaseModel):
+    filepath: str = Field(description="The path to the file to be modified")
+    task_description: str = Field(description="A detailed description of the task to be performed on the file, e.g. 'add user authentication', 'implement data processing logic', etc.")
+
+class TaskPlan(BaseModel):
+    implementation_steps: list[ImplementationTask] = Field(description="A list of steps to be taken to implement the task")
+    model_config = ConfigDict(extra="allow")
+
+
     
